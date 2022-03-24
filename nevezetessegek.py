b98a708269c931479d22d08a85c1248f36ee9aea
szaboemma123 <77670808+szaboemma123@users.noreply.github.com>
2022-03-24 09:27:51 +0100

    Update nevezetessegek.py

--- nevezetessegek.py
+++ nevezetessegek.py
@@ -4,11 +4,31 @@ adatok = []
 def f1(label):
     print(label)
     inputFile("nevezetesegek.txt")
+    print("\tFranciaország")
+    print("\tUSA")
+    print("\tKína")
+    print("\tMagyarország")
+    print("\tIndia")
+    print("\tAusztrália")
 
-inputFile(file):
+def f2(label):
+    print(label)
+    orszagnev = input("\tKérek egy országot: ")
+    for i in range(len(adatok)):
+        nev = adatok[i][0]
+        if(orszagnev == adatok):
+            print(adaok[i][0], adaok[i][1], adaok[i][2],)
+            
+      
+    
+def inputFile(file): 
     f = open(file, "r")
     for sor in f:
         sor = sor[0:-1].split(";")
         példány =n_class.Nevek(sor[0],sor[1],sor[2])
         adatok.append(példány)
     f.close()
+
+
+f1("1.feladat")
+f2("2.feladat")
